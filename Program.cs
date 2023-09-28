16be4288f71600c98aba5b58469c11ddf7054a2e
Josefin Mikaelsson <josefin.mikaelsson.NET23@edu.edugrade.se>
2023-09-28 14:08:24 +0200

    Added code thats ask for users name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Whats your name?");
+            string userName = Console.ReadLine();
         }
     }
 }
